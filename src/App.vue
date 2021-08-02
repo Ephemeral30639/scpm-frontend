f93550ea460795dbb253b7f0a6222f484009e3ec
Natdhanai.rat@student.mahidol.edu <natdhanai.rat@student.mahidol.edu>
2021-08-02 18:33:39 +0700

    Registration page implemented (submit validation not working)

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,8 @@
 
   <img alt="Project logo" src="./assets/logo.png" />
 
-  <!-- Top Navigation Bar -->
-  <el-menu
-    :default-active="activeIndex2"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b">
-    <el-menu-item index="1">Home</el-menu-item>
-    <el-submenu index="2">
-      <template #title>Features</template>
-      <el-menu-item index="2-1">Course and Credit Tracking</el-menu-item>
-      <el-menu-item index="2-2">Course Suggestions</el-menu-item>
-      <el-menu-item index="2-3">Generation Per Course Counter</el-menu-item>
-      <el-menu-item index="2-4">Makeup Finder</el-menu-item>
-    </el-submenu>
-    <el-menu-item index="3">Login</el-menu-item>
-    <el-menu-item index="4"><a href="/register" target="_blank">Register</a></el-menu-item>
-  </el-menu>
-
   <router-view/>
-
+  
 </template>
 
 <script setup>
@@ -32,6 +11,10 @@ import Home from './views/Home.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<script>
+
 </script>
 
 <style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,44 +1,144 @@
-
 <template>
-<el-row>
-  <el-button>Default</el-button>
-  <el-button type="primary">Primary</el-button>
-  <el-button type="success">Success</el-button>
-  <el-button type="info">Info</el-button>
-  <el-button type="warning">Warning</el-button>
-  <el-button type="danger">Danger</el-button>
-</el-row>
 
-<el-row>
-  <el-button plain>Plain</el-button>
-  <el-button type="primary" plain>Primary</el-button>
-  <el-button type="success" plain>Success</el-button>
-  <el-button type="info" plain>Info</el-button>
-  <el-button type="warning" plain>Warning</el-button>
-  <el-button type="danger" plain>Danger</el-button>
-</el-row>
+<!-- Top Navigation Bar -->
+<el-menu
+  :default-active="activeIndex"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1"><a href="/">Home</a></el-menu-item>
+  <el-submenu index="2">
+    <template #title>Features</template>
+    <el-menu-item index="2-1">Course and Credit Tracking</el-menu-item>
+    <el-menu-item index="2-2">Course Suggestions</el-menu-item>
+    <el-menu-item index="2-3">Generation Per Course Counter</el-menu-item>
+    <el-menu-item index="2-4">Makeup Finder</el-menu-item>
+  </el-submenu>
+  <el-menu-item index="3"><a href="/login">Login</a></el-menu-item>
+  <el-menu-item index="4"><a href="/register">Register</a></el-menu-item>
+</el-menu>
 
-<el-row>
-  <el-button round>Round</el-button>
-  <el-button type="primary" round>Primary</el-button>
-  <el-button type="success" round>Success</el-button>
-  <el-button type="info" round>Info</el-button>
-  <el-button type="warning" round>Warning</el-button>
-  <el-button type="danger" round>Danger</el-button>
-</el-row>
+<!-- Form -->
+<el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+  <el-form-item label="First Name" prop="firstName">
+    <el-input v-model="ruleForm.firstName"></el-input>
+  </el-form-item>
+  <el-form-item label="Last Name" prop="lastName">
+    <el-input v-model="ruleForm.lastName"></el-input>
+  </el-form-item>
+  <el-form-item label="Email" prop="email">
+    <el-input v-model="ruleForm.email"></el-input>
+  </el-form-item>
+  <el-form-item label="Password" prop="pass">
+    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+  </el-form-item>
+  <el-form-item label="Confirm" prop="checkPass">
+    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="submitForm('ruleForm')">Submit</el-button>
+    <el-button @click="resetForm('ruleForm')">Reset</el-button>
+  </el-form-item>
+</el-form>
 
-<el-row>
-  <el-button icon="el-icon-search" circle></el-button>
-  <el-button type="primary" icon="el-icon-edit" circle></el-button>
-  <el-button type="success" icon="el-icon-check" circle></el-button>
-  <el-button type="info" icon="el-icon-message" circle></el-button>
-  <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-  <el-button type="danger" icon="el-icon-delete" circle></el-button>
-</el-row>
 </template>
 
 <script>
-  
+  export default {
+    data() {
+      
+      // Form Data
+      var checkFirstName = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('Please input your first name'));
+        }
+      };
+      var checkLastName = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('Please input your last name'));
+        }
+      };
+      var checkEmail = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('Please input your email'));
+        }
+        if (!value.includes("student.mahidol.edu")){
+          return callback(new Error("Please use an EDU email"))
+        }
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input the password'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input the password again'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('Two inputs don\'t match!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        
+        // Top Navigation Bar Yellow Highlighting
+        activeIndex: '4',
+        
+        // Form Scripts
+        ruleForm: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          pass: '',
+          checkPass: ''
+        },
+        rules: {
+          firstName: [
+            { validator: checkFirstName, trigger: 'blur'}
+          ],
+          lastName: [
+            { validator: checkLastName, trigger: 'blur'}
+          ],
+          email: [
+            { validator: checkEmail, trigger: 'blur'}
+          ],
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
 </script>
 
 <style>
