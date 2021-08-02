0907ce6490f4cd851f5a45d9178449512344054b
Natdhanai.rat@student.mahidol.edu <natdhanai.rat@student.mahidol.edu>
2021-08-02 16:11:05 +0700

    routing implemented

--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue';
+import router from './router'
+
+// const Welcome = "./views/Welcome.vue"
+
+// const routes = [
+//     {path: '/welcome', component: Welcome}
+// ]
+
+// const router = VueRouter.createRouter({
+//     history: VueRouter.createWebHashHistory(),
+//     routes,
+// })
 
 const app = createApp(App)
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
