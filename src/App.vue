6c10f3e664a0443929aeb96676209889afe8683a
Natdhanai.rat@student.mahidol.edu <natdhanai.rat@student.mahidol.edu>
2021-08-02 14:05:26 +0700

    Implemented Feature Cards

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Project logo" src="./assets/logo.png" />
   <Home />
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,8 @@
-
 <template>
+
+<!-- Top Navigation Bar -->
 <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">Processing Center</el-menu-item>
+  <el-menu-item index="1">Home</el-menu-item>
   <el-submenu index="2">
     <template #title>Workspace</template>
     <el-menu-item index="2-1">item one</el-menu-item>
@@ -17,7 +18,81 @@
   <el-menu-item index="3" disabled>Info</el-menu-item>
   <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
 </el-menu>
-<div class="line"></div>
+
+<!-- Welcoming Text -->
+<h1>Simplify Your College Planning Experience</h1>
+<h3>The all in one tool to keep track of your studies</h3>
+
+<!-- Feature Card Container -->
+<el-row :gutter="20">
+
+  <!-- Feature Card 1 -->
+  <el-col :span="6">
+    <div class="grid-content bg-purple"></div>
+    <el-card :body-style="{ padding: '0px' }">
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <div style="padding: 14px;">
+        <h2>Course and Credit Tracking</h2>
+        <span>Keep track of the courses you have taken and the credits you have obtained so far</span>
+        <div class="bottom">
+          <time class="time">{{ currentDate }}</time>
+          <el-button type="text" class="button">Operating</el-button>
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+
+  <!-- Feature Card 2 -->
+  <el-col :span="6">
+    <div class="grid-content bg-purple"></div>
+    <el-card :body-style="{ padding: '0px' }">
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <div style="padding: 14px;">
+        <h2>Course Suggestions</h2>
+        <span>Suggest the next course you should take based on your taken courses</span>
+        <div class="bottom">
+          <time class="time">{{ currentDate }}</time>
+          <el-button type="text" class="button">Operating</el-button>
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+
+  <!-- Feature Card 3 -->
+  <el-col :span="6">
+    <div class="grid-content bg-purple"></div>
+    <el-card :body-style="{ padding: '0px' }">
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <div style="padding: 14px;">
+        <h2>GPCC</h2>
+        <span>Generation Per Course Counter. Check how many students in a generation has taken a specific course</span>
+        <div class="bottom">
+          <time class="time">{{ currentDate }}</time>
+          <el-button type="text" class="button">Operating</el-button>
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+
+  <!-- Feature Card 4 -->
+  <el-col :span="6">
+    <div class="grid-content bg-purple"></div>
+    <el-card :body-style="{ padding: '0px' }">
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <div style="padding: 14px;">
+        <h2>Makeup Finder</h2>
+        <span>Find available timeslots for your cancelled classes in an efficient manner</span>
+        <div class="bottom">
+          <time class="time">{{ currentDate }}</time>
+          <el-button type="text" class="button">Operating</el-button>
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+  
+</el-row>
+
+<!-- Button Navigation Bar -->
 <el-menu
   :default-active="activeIndex2"
   class="el-menu-demo"
@@ -26,7 +101,7 @@
   background-color="#545c64"
   text-color="#fff"
   active-text-color="#ffd04b">
-  <el-menu-item index="1">Processing Center</el-menu-item>
+  <el-menu-item index="1">Home</el-menu-item>
   <el-submenu index="2">
     <template #title>Workspace</template>
     <el-menu-item index="2-1">item one</el-menu-item>
@@ -42,6 +117,7 @@
   <el-menu-item index="3" disabled>Info</el-menu-item>
   <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
 </el-menu>
+
 </template>
 
 <script>
@@ -61,5 +137,26 @@
 </script>
 
 <style>
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
+  .button {
+    padding: 0;
+    min-height: auto;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
 </style>
