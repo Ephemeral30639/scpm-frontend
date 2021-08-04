75d31f038715faea8956c527a06cc9731819c767
Kongsin <kongsin33427@gmail.com>
2021-08-04 15:18:47 +0700

    link to makeup, makeup page

--- src/views/Makeup.vue
+++ src/views/Makeup.vue
@@ -14,30 +14,24 @@
     <template #title>Features</template>
     <el-menu-item index="2-1">Course and Credit Tracking</el-menu-item>
     <el-menu-item index="2-2">Course Suggestions</el-menu-item>
-    <el-menu-item index="2-3">Generation Per Course Counter</el-menu-item>
-    <el-menu-item index="2-4">Makeup Finder</el-menu-item>
+    <el-menu-item index="2-3"><a href="/GPCC">Generation Per Course Counter</a></el-menu-item>
+    <el-menu-item index="2-4"><a href="/Makeup">Makeup Finder</a></el-menu-item>
   </el-submenu>
   <el-menu-item index="3"><a href="/login">Login</a></el-menu-item>
   <el-menu-item index="4"><a href="/register">Register</a></el-menu-item>
 </el-menu>
 
+  <el-calendar>
+  <template #dateCell="{data}">
+    <p :class="data.isSelected ? 'is-selected' : ''">
+      {{ data.day.split('-').slice(1).join('-') }} {{ data.isSelected ? '❌' : '' }}
+    </p>
+  </template>
+</el-calendar>
 </template>
 
-<script>
-export default {
-    data() {
-      return {
-        activeIndex: '2',
-      };
-    },
-    methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
-    }
-  }
-</script>
-
 <style>
-
+  .is-selected {
+    color: #1989FA;
+  }
 </style>
