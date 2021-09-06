e442f9da1e63e572f2ef460d66031ba96b9d45c7
Apple <ekapat.lew@student.mahidol.edu>
2021-09-06 17:16:07 +0700

    fixed taken course page

--- src/views/TakenCourses.vue
+++ src/views/TakenCourses.vue
@@ -6,21 +6,21 @@
   <el-collapse v-model="activeNames" @change="handleChange" style="margin:30px;">
     <el-collapse-item title="Taken Required Courses" name="1">
       <!-- table -->
-      <el-table :data="takenRequiredCourses" stripe style="width: 100%; justify-content: space-between;"> 
+      <el-table :data="RequiredCourses" stripe style="width: 100%; justify-content: space-between;"> 
         <el-table-column prop="ID" label="Course ID" ></el-table-column>
         <el-table-column prop="Name" label="Name" ></el-table-column>
         <el-table-column prop="Credit" label="Credit"></el-table-column>
       </el-table>
-      <el-tag :type="success" style="margin-top:25px;">Total Credit Earned: {{totalCreditTable1}}</el-tag>
+      <el-tag :type="success" style="margin-top:25px;">Total Credit Earned: {{RequiredTotalCredit}}</el-tag>
     </el-collapse-item>
     <el-collapse-item title="Taken Elective Courses" name="2">
       <!-- table -->
-      <el-table :data="takenElectiveCourses" stripe style="width: 100%; justify-content: space-between;"> 
+      <el-table :data="ElectiveCourses" stripe style="width: 100%; justify-content: space-between;"> 
         <el-table-column prop="ID" label="Course ID" ></el-table-column>
         <el-table-column prop="Name" label="Name" ></el-table-column>
         <el-table-column prop="Credit" label="Credit"></el-table-column>
       </el-table>
-      <el-tag :type="success" style="margin-top:25px;">Total Credit Earned: {{totalCreditTable2}}</el-tag>
+      <el-tag :type="success" style="margin-top:25px;">Total Credit Earned: {{ElectiveTotalCredit}}</el-tag>
     </el-collapse-item>
   </el-collapse>
 
@@ -35,10 +35,24 @@ export default {
     data() {
       return {
         activeIndex: '2',
-        takenRequiredCourses:[],
-        takenElectiveCourses:[],
-        totalCreditTable1: 0,
-        totalCreditTable2: 0
+
+        CoreCourses:[],
+        RequiredCourses:[],
+        ElectiveCourses:[],
+        EnglishCourses:[],
+        NatSciCourses:[],
+        SocialSciCourses:[],
+        HumanityCourses:[],
+        PECourses:[],
+
+        CoreTotalCredit: 0,
+        RequiredTotalCredit: 0,
+        ElectiveTotalCredit: 0,
+        EnglishTotalCredit: 0,
+        NatSciTotalCredit: 0,
+        SocialSciTotalCredit: 0,
+        HumanityTotalCredit: 0,
+        PETotalCredit: 0
       };
     },
 
@@ -48,21 +62,46 @@ export default {
       // No cookies was send to the backend to identify the logged in user.
       axios.get('http://localhost:5000/taken-courses', {withCredentials: true})
             .then((res) => {
+            console.log("hello")
             console.log(res.data)
 
             //filter JSON data
-            this.takenRequiredCourses = res.data.filter(course => course.Category =="Required Major Courses")
+            this.CoreCourses = res.data.filter(course => course.Category =="Core Courses")
 
-            this.takenElectiveCourses = res.data.filter(course => course.Category =="Elective Major Courses")
+            this.RequiredCourses = res.data.filter(course => course.Category =="Required Major Courses")
+
+            this.ElectiveCourses = res.data.filter(course => course.Category =="Elective Major Courses")
+
+            this.EnglishCourses = res.data.filter(course => course.Category =="English Communication")
+                  
+            this.NatSciCourses = res.data.filter(course => course.Category =="Natural Sciences")
+
+            this.SocialSciCourses = res.data.filter(course => course.Category =="Social Sciences")
+                        
+            this.HumanityCourses = res.data.filter(course => course.Category =="Humanities")
+
+            this.PECourses = res.data.filter(course => course.Category =="Health Science and Physical Education")
 
             //sum JSON object keys
-            this.totalCreditTable1 = this.takenRequiredCourses.map(takenRequiredCourses => takenRequiredCourses.Credit).reduce((total, takenRequiredCourses) => takenRequiredCourses + total)
+            //Fixed reduce empty array by adding initial value ref:https://stackoverflow.com/questions/23359173/javascript-reduce-an-empty-array
+            this.CoreTotalCredit = this.CoreCourses.map(CoreCourses => CoreCourses.Credit).reduce((total, CoreCourses) => CoreCourses + total, 0)
 
-            this.totalCreditTable2 = this.takenElectiveCourses.map(takenElectiveCourses => takenElectiveCourses.Credit).reduce((total, takenElectiveCourses) => takenElectiveCourses + total)
+            this.RequiredTotalCredit = this.RequiredCourses.map(RequiredCourses => RequiredCourses.Credit).reduce((total, RequiredCourses) => RequiredCourses + total, 0)
 
-            console.log(this.totalCreditTable1)
-            console.log(this.totalCreditTable2)
+            this.ElectiveTotalCredit = this.ElectiveCourses.map(ElectiveCourses => ElectiveCourses.Credit).reduce((total, ElectiveCourses) => ElectiveCourses + total, 0)
 
+            this.EnglishTotalCredit = this.EnglishCourses.map(EnglishCourses => EnglishCourses.Credit).reduce((total, EnglishCourses) => EnglishCourses + total, 0)
+
+            this.NatSciTotalCredit = this.NatSciCourses.map(NatSciCourses => NatSciCourses.Credit).reduce((total, NatSciCourses) => NatSciCourses + total, 0)
+
+            this.SocialSciTotalCredit = this.SocialSciCourses.map(SocialSciCourses => SocialSciCourses.Credit).reduce((total, SocialSciCourses) => SocialSciCourses + total, 0)
+
+            this.HumanityTotalCredit = this.HumanityCourses.map(HumanityCourses => HumanityCourses.Credit).reduce((total, HumanityCourses) => HumanityCourses + total, 0)
+
+            this.PETotalCredit = this.PECourses.map(PECourses => RequiredCourses.Credit).reduce((total, PECourses) => PECourses + total, 0)
+
+            console.log(this.RequiredTotalCredit)
+            console.log(this.ElectiveTotalCredit)
 
             })
             .catch((err) => {
