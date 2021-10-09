b4c7a9868eabaaacc6b37b2a36e6208e67fc28b0
Natdhanai.rat@student.mahidol.edu <natdhanai.rat@student.mahidol.edu>
2021-10-09 12:06:22 +0700

    Code indentation Clean up

--- src/components/TakenCoursesComponent/TakenCourseGauge.vue
+++ src/components/TakenCoursesComponent/TakenCourseGauge.vue
@@ -2,26 +2,26 @@
     <el-col :span="span">
         <div class="grid-content bg-purple"></div>
         <el-card :body-style="{ padding: '0px' }">
-        <template #header>
-            <h3>{{title}}</h3>
-            <el-progress type="dashboard" :percentage="(obtainedCredit/totalCredit)*100" :color="colors">
-                <el-container direction="vertical">
-                    <span class="percentage-value" style="font-weight: bolder;">{{obtainedCredit}} / {{totalCredit}}</span>
-                    <br>
-                    <span style="font-weight: bolder;">Credits</span>
-                </el-container>
-            </el-progress>
-        </template>
-        <el-scrollbar height="200px">
-            <h1 v-show="checkEmpty()" style="margin-top: 80px; color: red;">No Course</h1>
-            <div v-for="o in courses.length" :key="o" style="padding-top: 10px;">
-            <el-container style="padding-left: 10px;">
-                <p>{{courses[parseInt(o-1)].ID}}</p>
-                <el-divider direction="vertical"></el-divider>
-                <p style="text-align: left; padding-right: 10px">{{courses[parseInt(o-1)].Name}} <strong>({{courses[parseInt(o-1)].Credit}})</strong></p>
-            </el-container>
-            </div>
-        </el-scrollbar>
+            <template #header>
+                <h3>{{title}}</h3>
+                <el-progress type="dashboard" :percentage="(obtainedCredit/totalCredit)*100" :color="colors">
+                    <el-container direction="vertical">
+                        <span class="percentage-value" style="font-weight: bolder;">{{obtainedCredit}} / {{totalCredit}}</span>
+                        <br>
+                        <span style="font-weight: bolder;">Credits</span>
+                    </el-container>
+                </el-progress>
+            </template>
+            <el-scrollbar height="200px">
+                <h1 v-show="checkEmpty()" style="margin-top: 80px; color: red;">No Course</h1>
+                <div v-for="o in courses.length" :key="o" style="padding-top: 10px;">
+                    <el-container style="padding-left: 10px;">
+                        <p>{{courses[parseInt(o-1)].ID}}</p>
+                        <el-divider direction="vertical"></el-divider>
+                        <p style="text-align: left; padding-right: 10px">{{courses[parseInt(o-1)].Name}} <strong>({{courses[parseInt(o-1)].Credit}})</strong></p>
+                    </el-container>
+                </div>
+            </el-scrollbar>
         </el-card>
     </el-col>
 </template>
--- src/components/TakenCoursesComponent/TakenCourseGeElectiveGauge.vue
+++ src/components/TakenCoursesComponent/TakenCourseGeElectiveGauge.vue
@@ -2,43 +2,43 @@
     <el-col :span="span">
         <div class="grid-content bg-purple"></div>
         <el-card :body-style="{ padding: '0px' }">
-        <template #header>
-            <h3>{{title}}</h3>
-            <el-container style="display: flex; justify-content: center;">
-                <el-space>
-                    <div>
-                    <el-progress type="dashboard" :percentage="calculate(obtainedCredit, totalCreditAdvance)" :color="colors">
-                        <el-container direction="vertical">
-                            <span class="percentage-value" style="font-weight: bolder;">{{obtainedCredit}} / {{totalCreditAdvance}}</span>
-                            <br>
-                            <span style="font-weight: bolder;">Credits</span>
-                        </el-container>
-                    </el-progress>
-                    <p><em>Advance Track</em></p>
-                    </div>
-                    <div>
-                    <el-progress type="dashboard" :percentage="calculate(obtainedCredit, totalCreditNormal)" :color="colors">
-                        <el-container direction="vertical">
-                            <span class="percentage-value" style="font-weight: bolder;">{{obtainedCredit}} / {{totalCreditNormal}}</span>
-                            <br>
-                            <span style="font-weight: bolder;">Credits</span>
-                        </el-container>
-                    </el-progress>
-                    <p><em>Normal Track</em></p>
-                    </div>
-                </el-space>
-            </el-container>
-        </template>
-        <el-scrollbar height="200px">
-            <h1 v-show="checkEmpty()" style="margin-top: 80px; color: red;">No Course</h1>
-            <div v-for="o in courses.length" :key="o" style="padding-top: 10px;">
-            <el-container style="padding-left: 10px;">
-                <p>{{courses[parseInt(o-1)].ID}}</p>
-                <el-divider direction="vertical"></el-divider>
-                <p style="text-align: left; padding-right: 10px">{{courses[parseInt(o-1)].Name}} <strong>({{courses[parseInt(o-1)].Credit}})</strong></p>
-            </el-container>
-            </div>
-        </el-scrollbar>
+            <template #header>
+                <h3>{{title}}</h3>
+                <el-container style="display: flex; justify-content: center;">
+                    <el-space>
+                        <div>
+                            <el-progress type="dashboard" :percentage="calculate(obtainedCredit, totalCreditAdvance)" :color="colors">
+                                <el-container direction="vertical">
+                                    <span class="percentage-value" style="font-weight: bolder;">{{obtainedCredit}} / {{totalCreditAdvance}}</span>
+                                    <br>
+                                    <span style="font-weight: bolder;">Credits</span>
+                                </el-container>
+                            </el-progress>
+                            <p><em>Advance Track</em></p>
+                        </div>
+                        <div>
+                            <el-progress type="dashboard" :percentage="calculate(obtainedCredit, totalCreditNormal)" :color="colors">
+                                <el-container direction="vertical">
+                                    <span class="percentage-value" style="font-weight: bolder;">{{obtainedCredit}} / {{totalCreditNormal}}</span>
+                                    <br>
+                                    <span style="font-weight: bolder;">Credits</span>
+                                </el-container>
+                            </el-progress>
+                            <p><em>Normal Track</em></p>
+                        </div>
+                    </el-space>
+                </el-container>
+            </template>
+            <el-scrollbar height="200px">
+                <h1 v-show="checkEmpty()" style="margin-top: 80px; color: red;">No Course</h1>
+                <div v-for="o in courses.length" :key="o" style="padding-top: 10px;">
+                    <el-container style="padding-left: 10px;">
+                        <p>{{courses[parseInt(o-1)].ID}}</p>
+                        <el-divider direction="vertical"></el-divider>
+                        <p style="text-align: left; padding-right: 10px">{{courses[parseInt(o-1)].Name}} <strong>({{courses[parseInt(o-1)].Credit}})</strong></p>
+                    </el-container>
+                </div>
+            </el-scrollbar>
         </el-card>
     </el-col>
 </template>
